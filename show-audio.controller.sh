9afe225ac4ce42a9d95b0b027bbcc6ff4c9517f7
Tony Tromp <tony.tromp@gmail.com>
2022-02-01 07:58:28 +0100

    Added TMUX help command to detach from Speaker Application

--- show-audio.controller.sh
+++ show-audio.controller.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Shows Audio Controller UI started within a running Tidal Container"
+echo ""
+echo "NOTE: Use Cntrl+b, d To detach from the Speaker Application. Cntrl+c will exit the Speaker Application.."
+sleep 2
 docker exec -ti tidal_connect /usr/bin/tmux a -t speaker_controller_application
